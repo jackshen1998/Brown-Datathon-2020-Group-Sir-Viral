5ca046bcf930f03aee53821da9ebedd8b5b84ff6
Caleb Sacks <16855387+clabe45@users.noreply.github.com>
2020-02-22 17:45:22 -0500

    Display text and images vertically

--- site/src/Section.js
+++ site/src/Section.js
@@ -4,15 +4,9 @@ class Section extends React.Component {
   render() {
     return (
       <div className="section">
-        <div className="columns is-desktop is-vcentered">
-          <div className="column">
-            <img src={this.props.image} />
-          </div>
-          <div className="column">
-            <div className="title">{this.props.title}</div>
-            <div className="subtitle">{this.props.body}</div>
-          </div>
-        </div>
+        <div className="title margin-below">{this.props.title}</div>
+        <div className="subtitle">{this.props.body}</div>
+        <img src={this.props.image} />
       </div>
     );
   }
