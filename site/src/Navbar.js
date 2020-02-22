b42f75d88647e3ebedc98cbb690b9e7c065e1fe3
Caleb Sacks <16855387+clabe45@users.noreply.github.com>
2020-02-22 18:35:04 -0500

    Make logo clickable

--- site/src/Navbar.js
+++ site/src/Navbar.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import './Navbar.css';
 
 class Navbar extends React.Component {
   render() {
     return (
       <div className="navbar">
         <div className="navbar-brand">
-          <img src={this.props.logo} width="215px" height="40px" />
+          <a href="/">
+            <img src={this.props.logo} width="215px" height="40px" />
+          </a>
         </div>
       </div>
     )
